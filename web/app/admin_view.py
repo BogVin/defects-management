04618f87c27c5b22e3876629d29c42035a7d4d49
BogVin <spongebob69961@gmail.com>
2021-02-14 21:20:01 +0200

    base api for users and defects

---
+++ web/app/admin_view.py
@@ -0,0 +1,23 @@
+from app import models
+from flask import jsonify
+from flask_restful import Resource, fields, marshal_with, abort,  reqparse
+from flask_jwt_extended import create_access_token
+
+admin_puts_args = reqparse.RequestParser()
+admin_puts_args.add_argument("email", type=str, help="Enter email", required=True)
+admin_puts_args.add_argument("password", type=str, help="Enter password", required=True)
+
+
+class AdminLogin(Resource):
+    def post(self):
+        args = admin_puts_args.parse_args()
+        email = args["email"]
+        password = args["password"]
+        admin = models.Admin.query.filter_by(email=email).first()
+        if not admin:
+            return abort(401, message="Wrong email")
+        if not password or not admin.check_password(password):
+            return abort(401, message="Wrong password")
+
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token)
--- web/app/telegram_users_view.py
+++ web/app/telegram_users_view.py
@@ -1,65 +1,75 @@
 from flask_restful import Resource, fields, marshal_with, abort,  reqparse
 from app import models
 from app import db
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask import jsonify
 
 
 class RoleItem(fields.Raw):
     def format(self, value):
         return value.value
 
+
 resource_fields = {
-    'id': fields.Integer,
+    'telegram_id': fields.String,
     'first_name': fields.String,
     'last_name': fields.String,
     'username': fields.String,
-    "phone": fields.String,
-    "role": RoleItem,
+    'role': RoleItem,
+    'is_active': fields.Boolean
 }
 
 user_puts_args = reqparse.RequestParser()
-user_puts_args.add_argument("id", type=int, help="Enter user id", required=True)
+user_puts_args.add_argument("telegram_id", type=str, help="Enter user id", required=True)
 user_puts_args.add_argument("first_name", type=str, help="User name", required=True)
 user_puts_args.add_argument("last_name", type=str, help="Last name", required=True)
 user_puts_args.add_argument("username", type=str, help="Username", required=True)
-user_puts_args.add_argument("phone", type=str, help="phone number", required=True)
 
 
 user_update_args = reqparse.RequestParser()
-user_update_args.add_argument("first_name", type=str, help="Name")
-user_update_args.add_argument("last_name", type=str, help="Last name")
-user_update_args.add_argument("username", type=str, help="Username ")
-user_update_args.add_argument("role", type=str, help="role")
+user_update_args.add_argument("first_name", type=str, help="Name", required=True)
+user_update_args.add_argument("last_name", type=str, help="Last name", required=True)
+user_update_args.add_argument("username", type=str, help="Username ", required=True)
+user_update_args.add_argument("role", type=str, help="Role", required=True)
+user_update_args.add_argument("is_active", type=bool, help="Is active", required=True)
+
 
 class TUserList(Resource):
+    @jwt_required
     @marshal_with(resource_fields)
     def get(self):
-        users = models.TelegramUser.query.all()
-        if not users:
-            abort(404, message="Users not found")
-        return users
+        current_user = get_jwt_identity()
+        if isinstance(current_user, str) and models.Admin.query.filter_by(email=current_user).first():
+            users = models.TelegramUser.query.all()
+            if not users:
+                abort(404, message="Users not found")
+            return users
+        else:
+            abort(401)
+
+    @marshal_with(resource_fields)
+    def post(self):
+        args = user_puts_args.parse_args()
+        result = models.TelegramUser.query.filter_by(telegram_id=args["telegram_id"]).first()
+        if result:
+            abort(409, message="User already exist")
+
+        user = models.TelegramUser(telegram_id=args['telegram_id'], first_name=args['first_name'], last_name=args['last_name'], username=args['username'],)
+        db.session.add(user)
+        db.session.commit()
+        return user, 201
 
 
 class TUser(Resource):
+    @jwt_required
     @marshal_with(resource_fields)
-    def get(self,user_id):
+    def get(self, user_id):
         user = models.TelegramUser.query.get(user_id)
         if not user:
             abort(404, message="User not found")
         return user
 
-    @marshal_with(resource_fields)
-    def post(self, user_id):
-        args = user_puts_args.parse_args()
-        result = models.TelegramUser.query.filter_by(id=user_id).first()
-        if result:
-            abort(409, message="User already exist")
-
-        user = models.TelegramUser(id=args['id'], first_name=args['first_name'], last_name=args['last_name'], username=args['username'],
-                                   phone=args['phone'])
-        db.session.add(user)
-        db.session.commit()
-        return user, 201
-
+    @jwt_required
     @marshal_with(resource_fields)
     def put(self, user_id):
         args = user_update_args.parse_args()
@@ -67,19 +77,17 @@ class TUser(Resource):
         if not user:
             abort(404, message="User doesn't exist, cannot update")
 
-        if args['first_name']:
-            user.first_name = args['first_name']
-        if args['last_name']:
-            user.last_name = args['last_name']
-        if args['username']:
-            user.username = args['username']
-        if args['role']:
-            user.role = args['role']
+        user.first_name = args['first_name']
+        user.last_name = args['last_name']
+        user.username = args['username']
+        user.role = args['role']
+        user.is_active = args['is_active']
 
         db.session.commit()
 
         return user
 
+    @jwt_required
     def delete(self, user_id):
         user = models.TelegramUser.query.get(user_id)
         if not user:
@@ -87,3 +95,15 @@ class TUser(Resource):
         db.session.delete(user)
         db.session.commit()
         return {"message": "User deleted successfully"}
+
+
+class TUserLogin(Resource):
+    def post(self, telegram_id):
+        user = models.TelegramUser.query.filter_by(telegram_id=telegram_id).first()
+        if not user:
+            abort(401, message="Wrong user id")
+        if user.is_active:
+            access_token = create_access_token(identity=telegram_id)
+            return jsonify(access_token=access_token)
+        else:
+            return {"message": "You not active, wait for you activation"}
